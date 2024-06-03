5e6254714f87ab9905ed89aca506d48ba01ab23d
cxp <cxp030509@gmail.com>
2024-06-04 05:53:10 +0800

    cicd

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ def generate_content():
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/api/ai/updateAPI', methods=['PUT'])
-def update_api_key():
-    global model
-    data = request.json
-    os.environ['API_KEY'] = data.get('apiKey')
-    genai.configure(api_key=os.environ['API_KEY'])
-    model = genai.GenerativeModel(model_name='gemini-1.5-flash')
-    return jsonify({'message': 'API Key updated successfully'})
+# @app.route('/api/ai/updateAPI', methods=['PUT'])
+# def update_api_key():
+#     global model
+#     data = request.json
+#     os.environ['API_KEY'] = data.get('apiKey')
+#     genai.configure(api_key=os.environ['API_KEY'])
+#     model = genai.GenerativeModel(model_name='gemini-1.5-flash')
+#     return jsonify({'message': 'API Key updated successfully'})
 
 
 if __name__ == '__main__':
